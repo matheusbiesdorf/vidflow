98be158d6912fec959529dcb8d8d24eb8e79d6be
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-05 19:21:34 -0300

    Utilizando GIST

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const containerVideos = document.querySelector(".videos__container");
 
+//const url = "http://localhost:3000/videos"
+const url_com_gist = "https://gist.githubusercontent.com/antonio-evaldo/e8a63621b51c883931eb3fa3a3eca990/raw/12f5c46ee6dd00d03c051adadaf341e06452cea0/videos.txt"
+
+//https://gist.githubusercontent.com/antonio-evaldo/e8a63621b51c883931eb3fa3a3eca990/raw/12f5c46ee6dd00d03c051adadaf341e06452cea0/videos.txt
+
 
 async function buscarEMostrarVideos(){
     try{
-        const busca = await fetch("http://localhost:3000/videos");
+        const busca = await fetch(`${url_com_gist}`);
         const videos = await busca.json();
 
             videos.forEach((video)=> {
